b73025faaf5048c11c878fa18d2d0eb31cfa2fbe
Sander <tarssander1@gmail.com>
2015-12-27 02:11:55 +0200

    Added moving up and down by using transpose function. Added lines to test it. Changed Laud file also to test veritcal moving.

--- prog.hs
+++ prog.hs
@@ -1,7 +1,8 @@
 import Data.List.Split
+import Data.List
 
 fancyPrint [] = ""
-fancyPrint (x:xs) =  x ++ " " ++ (fancyPrint xs)
+fancyPrint (x:xs) =  (x ++ "\n") ++ (fancyPrint xs)
 
 count [] _ = 0
 count (x:xs) v
@@ -21,16 +22,24 @@ movex (x:xs) a s
             moveNowLeft (x:y:xs) a
               | x == ' ' && y == a = a : moveNowLeft (x:xs) a
               | otherwise = x : moveNowLeft (y:xs) a
+movey [] a s = []
+movey (x:xs) a s
+    | s == 'u' = transpose $ movex (transpose (x:xs)) a 'l'
+    | s == 'd' = transpose $ movex (transpose (x:xs)) a 'r'
 
 main :: IO ()
 main = do
     { content <- readFile "laud.txt"
     ; let lines = splitOn "\n" content
-    ; print (fancyPrint lines)
-    ; print (fancyPrint $ movex lines '1' 'l')
-    ; print (fancyPrint $ movex lines '1' 'r')
-    ; print (fancyPrint $ movex lines '3' 'r')
-    ; print (fancyPrint $ movex lines '3' 'l')
-    ; print (fancyPrint lines)
-    ; print (count lines '1')
+    ; putStrLn (fancyPrint lines)
+    ; putStrLn (fancyPrint $ movex lines '1' 'l')
+    ; putStrLn (fancyPrint $ movex lines '1' 'r')
+    ; putStrLn (fancyPrint $ movex lines '3' 'r')
+    ; putStrLn (fancyPrint $ movex lines '3' 'l')
+    ; putStrLn (fancyPrint $ movey lines '2' 'u')
+    ; putStrLn (fancyPrint $ movey lines '2' 'd')
+    ; putStrLn (fancyPrint $ movey lines '4' 'd')
+    ; putStrLn (fancyPrint $ movey lines '4' 'u')
+    ; putStrLn (fancyPrint lines)
+    ; print(count lines '1')
     }
